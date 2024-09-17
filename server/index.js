893000fdda9ad1fd67e754b06ee759d70130fcb7
chriscalver <130410868+chriscalver@users.noreply.github.com>
2024-09-17 18:17:36 -0400

    sd

--- server/index.js
+++ server/index.js
@@ -13,27 +13,41 @@ const io = new Server(server, {
   },
 });
 
-io.on("connection", (socket) => {
+var username = "";
+let chatlogs = [
+  {
+    room: "99",
+    author: "Chris",
+    message: "Welcome...",
+    time: "00:01"
+  },
+];
 
+console.log(chatlogs);
+
+io.on("connection", (socket) => {
   console.log(`User Connected: ${socket.id}`);
+  // console.log(socket);
 
   socket.on("join_room", (data) => {
-    console.log(data);
+    // console.log(data);
     socket.join(data);
     console.log(`User with ID: ${socket.id} joined room: ${data}`);
   });
-  
+
   socket.on("get_name", (data) => {
-    console.log("name:");
-
-    console.log(data);
-   // socket.join(data);
-   // console.log(`User with ID: ${socket.id} joined room: ${data}`);
+    console.log("name:" + data);
+    username = data;
+    // console.log(data);
+    // socket.join(data);
+    // console.log(`User with ID: ${socket.id} joined room: ${data}`);
   });
-
 
   socket.on("send_message", (data) => {
     socket.to(data.room).emit("receive_message", data);
+    // console.log(data);
+    chatlogs.push(data);
+    console.log(chatlogs);
   });
 
   socket.on("disconnect", () => {
@@ -43,5 +57,5 @@ io.on("connection", (socket) => {
 });
 
 server.listen(8080, () => {
-  console.log("Server listening on port 8080");
+  console.log("Server listening on port: 8080");
 });
