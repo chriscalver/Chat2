edee9280f02d194257e24cf51885869623f239e3
chriscalver <130410868+chriscalver@users.noreply.github.com>
2024-09-18 15:57:05 -0400

    p3

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "https://chat2-1-lw5o.onrender.com",
+    origin: "https://chriscalver.com/racer",
     methods: ["GET", "POST"],
   },
 });
