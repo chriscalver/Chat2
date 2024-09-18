daa55fd8ae728a6c7a51b7c68fd8a3ca76dbc345
chriscalver <130410868+chriscalver@users.noreply.github.com>
2024-09-18 16:03:50 -0400

    p4

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "https://chriscalver.com/racer",
+    origin: "https://chriscalver.com",
     methods: ["GET", "POST"],
   },
 });
